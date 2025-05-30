d2d3aabc894c6d51d63a8b124972647bb9f4ad20
git-Sahitya <sahitya5289@gmail.com>
2025-05-30 17:41:02 +0530

    Implement user signup functionality with validation and routing

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -1,5 +1,31 @@
-import User from "../models/user.model.js"
+import User from "../models/user.model.js";
 
-const signup = async (req,res) => {
-    const {name , email , password , confirmpassword} = req.body
-}
+export const signup = async (req, res) => {
+  try {
+    const { name, email, password, confirmpassword } = req.body;
+    if (password !== confirmpassword) {
+      return res.status(400).json({ message: "Password do not match" });
+    }
+    const user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({ message: "email already exist." });
+    }
+
+    const newUser = await new User({
+      name,
+      email,
+      password,
+    });
+
+    newUser
+      .save()
+      .then(() =>
+        res.status(201).json({ message: "User registered successfully" })
+      );
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectToDb from "./utils/db.js";
+import userRoute from "./router/user.router.js";
 
 const app = express();
 dotenv.config();
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 connectToDb();
 
@@ -12,6 +16,8 @@ const PORT = process.env.PORT || 5001;
 app.get("/", (req, res) => {
   res.send("Hello Backend");
 });
+
+app.use("/user", userRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
