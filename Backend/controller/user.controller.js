d39d3e2ebc0a1bc8daa1ef629f272030dd00f6f8
git-Sahitya <sahitya5289@gmail.com>
2025-06-05 18:27:02 +0530

    logout controller

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -57,3 +57,13 @@ export const login = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
+
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("jwt");
+    res.status(200).json({ message: "User logged out successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
