59e9ffc6a225783f68ee4d271805328bc4144cdb
git-Sahitya <sahitya5289@gmail.com>
2025-06-11 19:06:09 +0530

    create a router for signup , login and home page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,20 +5,37 @@ import Logout from "./Home/left1/Logout";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 import { useAuth } from "./context/AuthProvider";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 const App = () => {
-   const [authUser, setAuthUser] = useAuth();
+  const [authUser, setAuthUser] = useAuth();
   console.log(authUser);
   return (
     <>
-      {/* <div className='flex h-screen' >
-      <Logout></Logout>
-      <Left></Left>
-      <Right></Right>
-    </div> */}
-
-      {/* <Login/> */}
-      <Signup />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            authUser ? (
+              <div className="flex h-screen">
+                <Logout></Logout>
+                <Left></Left>
+                <Right></Right>
+              </div>
+            ) : (
+              <Navigate to={"/login"} />
+            )
+          }
+        />
+        <Route
+          path="/signup"
+          element={authUser ? <Navigate to={"/"} /> : <Signup />}
+        />
+        <Route
+          path="/login"
+          element={authUser ? <Navigate to={"/"} /> : <Login />}
+        />
+      </Routes>
     </>
   );
 };
