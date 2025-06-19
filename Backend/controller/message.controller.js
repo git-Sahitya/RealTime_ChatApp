6323dfee9a753a22a2199f1a3dad71fb41d8dd09
git-Sahitya <sahitya5289@gmail.com>
2025-06-19 19:21:16 +0530

    feat: Update message controller and routes; implement message retrieval and fix participant references

--- Backend/controller/message.controller.js
+++ Backend/controller/message.controller.js
@@ -8,14 +8,13 @@ export const sendMessage = async (req, res) => {
     const senderId = req.user._id; // current logged in user id
 
     let conversation = await Conversation.findOne({
-      participants: {
+      members: {
         $all: [senderId, receiverId],
       },
     });
     if (!conversation) {
       conversation = await Conversation.create({
-        participants: [senderId, receiverId],
-        messages: [{ sender: senderId, message }],
+        members: [senderId, receiverId],
       });
       const newMessage = new Message({
         senderId,
@@ -35,5 +34,23 @@ export const sendMessage = async (req, res) => {
   } catch (error) {
     console.log("Error in sending message:", error);
     res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const getMessage = async (req, res) => {
+  try {
+    const { id: chatUser } = req.params;
+    const senderId = req.user._id;
+
+    const conversation = await Conversation.findOne({
+      members: { $all: [senderId, chatUser] },
+    }).populate("message");
+    if (!conversation) {
+      return res.status(201).json({ message: "No conversation found" });
+    }
+    const message = conversation.message;
+    res.status(201).json({ message });
+  } catch (error) {
+    console.log("Message getting error", error);
   }
 };
